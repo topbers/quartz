4e8ac71f737b80aa759e94e47f5bb1bf9fd4940a
topbers <versabers@gmail.com>
2025-09-18 00:19:26 +0900

    Quartz sync: Sep 18, 2025, 12:19 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -57,7 +57,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["git", "filesystem", "frontmatter"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
