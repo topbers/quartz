dddaeb9d8f5b29140945fc62037397f0fa2a51d2
topbers <versabers@gmail.com>
2025-10-28 22:40:29 +0900

    Quartz sync: Oct 28, 2025, 10:40 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -52,6 +52,7 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="l-0CgXesY2tRk8D--UXxT9NOmjrb3tcXQes7m61CKeg" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
